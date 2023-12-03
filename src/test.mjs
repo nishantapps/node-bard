5c8457c2b9839c9cf8c16c7ad1fb39418aef87fd
Nishant Sharma <nishantapps55@gmail.com>
2023-12-03 14:22:42 +0530

    Update test.mjs

--- src/test.mjs
+++ src/test.mjs
@@ -1,8 +1,8 @@
 import {setConfig,createText} from './index.mjs'
 const config = {
-    userid:'RK2NpysYNOWK2U2HtFlC',
-    key:'AIzaSyDTFn6X_PSdpDwm8szBlbIAtUuPrg21dZo',
-    apikey:'3482f1a70emshd44e76978f99178p19e72djsn68a544565b42'
+    userid:'userid',
+    key:'makersuite.google.com key',
+    apikey:'rapidapi key'
 };
 
 setConfig(config);
